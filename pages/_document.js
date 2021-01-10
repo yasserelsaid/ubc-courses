0a70d05c12fdbd479eeaad148b873a8338484bf4
Yasser Elsaeed <yassermelsaeed@gmail.com>
2021-01-10 14:34:10 +0200

    added buy me a coffee

--- pages/_document.js
+++ pages/_document.js
@@ -36,6 +36,17 @@ export default class MyDocument extends Document {
       `,
             }}
           />
+          <script
+            data-name='BMC-Widget'
+            src='https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js'
+            data-id='otoosi'
+            data-description='Support me on Buy me a coffee!'
+            data-message='Thank you for visiting. If you like this website, and want to help with the hosting and database costs, consider buying me a coffee!'
+            data-color='#5F7FFF'
+            data-position='Right'
+            data-x_margin='18'
+            data-y_margin='18'
+          ></script>
         </Head>
         <body>
           <Main />
