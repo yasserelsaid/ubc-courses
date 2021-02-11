dc84606d5f269e186a43881205ba9835bf49da86
Yasser Elsaeed <yassermelsaeed@gmail.com>
2021-02-11 17:44:35 +0200

    added side ads

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,28 @@
 import Header from './header';
 import Footer from './footer';
+import AdSense from 'react-adsense';
+import Hidden from '@material-ui/core/Hidden';
 
 export default function Layout({ children }) {
   return (
     <div>
       <Header />
+      <Hidden mdDown>
+        <div>
+          <AdSense.Google
+            client='ca-pub-9351737408787682'
+            slot='9639084048'
+            format='auto'
+            style={{ width: 300, height: 500, float: 'left', padding: '10px' }}
+          />
+          <AdSense.Google
+            client='ca-pub-9351737408787682'
+            slot='9639084048'
+            format='auto'
+            style={{ width: 300, height: 500, float: 'right', padding: '10px' }}
+          />
+        </div>
+      </Hidden>
 
       <main>{children}</main>
 
